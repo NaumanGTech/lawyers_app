9edaaf0344915eb450a0ef12f1b7fecfd82759c7
Junaid-ur-Rehman <rehmanjunaid215@gmail.com>
2023-06-27 02:36:07 +0500

    customer profile update done

---
+++ app/Http/Middleware/Customer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class Customer
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user_id = auth()->user()->id;
+        $user = User::where('id', $user_id)->first();
+        if (Auth::user()->role == 'user') {
+            return $next($request);
+        } else if ((Auth::user()->role !== 'user')) {
+            return redirect()->back()->with('error', 'You do not have access of this page');
+        } else {
+            return redirect('/login');
+        }
+    }
+}
